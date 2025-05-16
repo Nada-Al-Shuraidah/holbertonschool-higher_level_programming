735e72734868edf42fba4e5996e06633a491b5a1
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-05-16 19:10:18 +0000

    Say my name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module contains the function say_my_name that prints a formatted full name.
+"""
+
+
 def say_my_name(first_name, last_name=""):
     """
     Prints My name is <first name> <last name>
