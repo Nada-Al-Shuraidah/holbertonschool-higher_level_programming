9c2e88e0765d5efc79cd56038c38a7d473eaeeff
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-05-16 19:06:58 +0000

    Say my name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def say_my_name(first_name, last_name=""):
+    """
+    Prints My name is <first name> <last name>
+
+    Args:
+        first_name (str): first name
+        last_name (str): last name (default is empty string)
+
+    Raises:
+        TypeError: if first_name or last_name is not a string
+    """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
