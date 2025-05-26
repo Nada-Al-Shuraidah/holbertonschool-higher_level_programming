eb5ea9542239d66b8b1f729536f65605ee4e855d
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-05-26 14:46:24 +0000

    Update

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,7 +5,7 @@
 class MyList(list):
     """
     MyList is a subclass of the built-in list class.
- 
+
     It adds a method that prints the list in sorted (ascending) order
     without modifying the original list.
     """
