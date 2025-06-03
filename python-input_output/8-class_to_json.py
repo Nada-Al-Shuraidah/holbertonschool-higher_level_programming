a625112094dd5e0e2d663ef77c4a011016dbf5a3
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-06-03 14:59:04 +0000

    Class to JSON

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Module that converts class instances to dictionary for JSON serialization."""
+
 def class_to_json(obj):
     """Return the dictionary description of an object for JSON serialization."""
     return obj.__dict__
