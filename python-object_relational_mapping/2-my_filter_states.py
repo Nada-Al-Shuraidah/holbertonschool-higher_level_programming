02a8c59a2231334bb958da45aed89c9015226d04
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-06-25 09:42:08 +0000

    Update

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
 """
-Lists all states from the database hbtn_0e_0_usa where name matches the argument
+Lists all states from the database hbtn_0e_0_usa
+where name matches the argument (unsafe version)
 """
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    user = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=user, passwd=password, db=db_name,
-                         charset="utf8")
-
-    cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name.replace("'", "''"))
-    cursor.execute(query)
-
-    for row in cursor.fetchall():
+    conn = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8"
+    )
+    cur = conn.cursor()
+    # لا تضيف escaping يدوي، خله زي كذا بالضبط
+    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-
-    cursor.close()
-    db.close()
+    cur.close()
+    conn.close()
